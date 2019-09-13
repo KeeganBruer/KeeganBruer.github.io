6751842597db7f2bd3ad34698bc1e421a84a2d8c
KeeganBruer <keegan.bruer@gmail.com>
2019-09-13 17:18:01 -0400

    Update projects.js

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	var navPages = document.getElementsByClassName('projectLink');
-
+	
 	navPages[0].onclick = function() {
 		window.location.href = "https://www.npmjs.com/package/discord-webface";
 	};
 	navPages[1].onclick = function() {
-		window.location.href = "https://github.com/KeeganBruer/Sheets";
+		window.location.href = "https://github.com/KeeganBruer/javadoc-generator";
 	};
 	navPages[2].onclick = function() {
-		window.location.href = "https://github.com/KeeganBruer/javadoc-generator";
+		window.location.href = "https://github.com/KeeganBruer/Sheets";
 	};
 });
